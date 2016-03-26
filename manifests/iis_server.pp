e68504c988310522a17807f2acb8eaff54a308bd
Ramesh Sencha <sencha@Rameshs-MacBook-Pro.local>
2016-03-27 01:04:33 +0530

    IIS server manifest

--- manifests/iis_server.pp
+++ manifests/iis_server.pp
@@ -1,11 +1,32 @@
 class puppet_module::iis_server{
  
-  # install iis server
+  file {'C:\GTechConfigFiles':
+    ensure => directory,
+  }
+
+  file {'IIS Installer':
+    path     => 'C:\GTechConfigFiles\iisexpress_1_11_x86_en-US.msi',
+    source   => "puppet:///modules/puppet_module/iisexpress_1_11_x86_en-US.msi",
+    owner    => 'Administrators',
+    group    => 'Administrators',
+    mode     => 0770, 
+    provider => windows,
+    ensure   => present,
+    require  => File['C:\GTechConfigFiles'],
+  }
+
   package { "IIS 7.5 Express":
     ensure          => installed,
-    source          => puppet:///modules/puppet_module/iisexpress_1_11_x86_en-US.msi,
+    source          => 'C:/Users/Administrator/iisexpress_1_11_x86_en-US.msi',
     install_options => ['/qn'],
     provider => windows,
+    require  => File['IIS Installer'],
+  }
+
+  file { 'IIS Installer Remove':
+    path    => 'C:\GTechConfigFiles\iisexpress_1_11_x86_en-US.msi',
+    ensure => absent,
+    require  => Package['IIS 7.5 Express'],
   }
 
 }
