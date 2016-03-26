a12e76fc049f706deb84efeb3df82d4fa280da24
Ramesh Sencha <sencha@Rameshs-MacBook-Pro.local>
2016-03-27 04:12:46 +0530

    added NTP server config

--- manifests/ntp_server.pp
+++ manifests/ntp_server.pp
@@ -10,15 +10,22 @@ class puppet_module::ntp_server {
     require  => File["C:\\GTechConfigFiles"],
   }
 
-  package { "NTP Server install" :
-    ensure          => installed,
-    source          => "C:\\GTechConfigFiles\\ntp-4.2.8p5-win32-setup.exe",
-    install_options => ['/qn', '/VERYSILENT',
-      { '/l' => 'Program Files\\NTP\\ntp.log' },
-      { 'INSTALLLOCATION' => 'C:\\Program Files\\NTP' },
-    ],
-    provider => windows,
+  exec{ "NTP Server install" :
+    command  => 'cmd /c "C:\\GTechConfigFiles\\ntp-4.2.8p5-win32-setup.exe" /q /norestart /S',
+    timeout  => 1800,
+    provider => powershell,
     require  => File["NTP Installer"],
   }
+
+  # package { "NTP Server install" :
+  #   ensure          => installed,
+  #   source          => "C:\\GTechConfigFiles\\ntp-4.2.8p5-win32-setup.exe",
+  #   install_options => ['/qn', '/VERYSILENT',
+  #     { '/l' => 'Program Files\\NTP\\ntp.log' },
+  #     { 'INSTALLLOCATION' => 'C:\\Program Files\\NTP' },
+  #   ],
+  #   provider => windows,
+  #   require  => File["NTP Installer"],
+  # }
 
 }
