7703bc6a02152ff9c4e4b10ec1197ad94e300c1a
Ramesh Sencha <rameshsencha7@gmail.com>
2016-03-26 23:38:17 +0530

    Update iis_server.pp
    corrected module name

--- manifests/iis_server.pp
+++ manifests/iis_server.pp
@@ -3,7 +3,7 @@ class iis_server{
   # install iis server
   package { "IIS 7.5 Express":
     ensure          => installed,
-    source          => puppet:///modules/dotnet1/iisexpress_1_11_x86_en-US.msi,
+    source          => puppet:///modules/puppet_module/iisexpress_1_11_x86_en-US.msi,
     install_options => ['/qn'],
     provider => windows,
   }
