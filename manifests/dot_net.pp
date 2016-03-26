185b3ff21afef1fefa657a5660a71ca188b33d6d
Ramesh Sencha <sencha@Rameshs-MacBook-Pro.local>
2016-03-27 02:52:10 +0530

    Dot net installer

--- manifests/dot_net.pp
+++ manifests/dot_net.pp
@@ -22,8 +22,8 @@ class puppet_module::dot_net {
     command  => '${Sys32}\\cmd.exe /c "C:\GTechConfigFiles\dotNetFx45_Full_setup.exe" /q /norestart | exit 0',
     creates  => "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild\Microsoft.Build.Core.xsd",
     timeout  => 1800,
-    provider => powershell
-    require  => package["Dot Net Installer"]
+    provider => powershell,
+    require  => package["Dot Net Installer"],
   }
 
 }
