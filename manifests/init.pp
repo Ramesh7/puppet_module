bdc19a66ac30b228eb4ecf3a94aae777fb8f2953
Ramesh Sencha <sencha@Rameshs-MacBook-Pro.local>
2016-04-03 16:09:23 +0530

    intendation fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,14 @@ class puppet_module {
     ensure => directory,
   }
 
-#   include puppet_module::iis_server
-#    include puppet_module::dot_net
-#   include puppet_module::ie_browser
-#   include puppet_module::ntp_server
-#    include puppet_module::smtp_server
-#    include puppet_module::odac_client
-#   include puppet_module::oracle_config  
-#    include puppet_module::msmq
+  include puppet_module::iis_server
+  include puppet_module::dot_net
+  # include puppet_module::ie_browser
+  include puppet_module::ntp_server
+  include puppet_module::smtp_server
+  include puppet_module::msmq
+  include puppet_module::odac_client
+  include puppet_module::oracle_client
+  include puppet_module::oracle_db_server
+
 }
---
+++ manifests/oracle_db_server.pp
@@ -0,0 +1,31 @@
+class puppet_module::oracle_db_server {
+
+  file { "C:\\CommandCenterInstaller\\OracleDBServer" :
+    ensure => directory,
+    require  => File["C:\\CommandCenterInstaller"],
+  }
+
+  file { "Copy oracle_db_Server resp" :
+    path     => "C:\\CommandCenterInstaller\\OracleDBServer\\OracleDBResponseFile.rsp",
+    source   => "puppet:///modules/puppet_module/OracleDBResponseFile.rsp",
+    owner    => "Administrators",
+    group    => "Administrators",
+    mode     => "0770", 
+    ensure   => present,
+    require  => File["C:\\CommandCenterInstaller\\OracleDBServer"],
+    before  => exec["Install Oracle DB Server"]
+  }
+     
+   exec { 'Install Oracle DB Server':
+     command   => '  
+       PUSHD C:\\CommandCenterInstaller\\OracleDBServer\\win64_11gR2_database_1of2\\database
+       cmd /c setup.exe -silent -nowait -noconfig -responseFile "C:\\CommandCenterInstaller\\OracleDBServer\\OracleDBResponseFile.rsp" 
+       Start-Sleep 700
+       }
+      ',
+       creates => 'C:\\Oracle\\product\\12.1.0\\client_1\\network\\admin\\tnsnames.ora',
+       timeout => 1200,
+       provider => powershell
+   }  
+
+}
