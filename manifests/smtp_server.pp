9dec439e6af5b9f7188c0083cde9e736a5668db6
Ramesh Sencha <sencha@Rameshs-MacBook-Pro.local>
2016-04-03 15:29:52 +0530

    Enable smtp service

--- manifests/smtp_server.pp
+++ manifests/smtp_server.pp
@@ -1,51 +1,56 @@
 class puppet_module::smtp_server {
 
-  file { "C:\\CommandCenterInstaller\\WinSMTPServer" :
-    ensure => directory,
-    require => File["C:\\CommandCenterInstaller"],
-  }
+  # file { "C:\\CommandCenterInstaller\\WinSMTPServer" :
+  #   ensure => directory,
+  #   require => File["C:\\CommandCenterInstaller"],
+  # }
 
-  file { "SMTP Installer" :
-    path     => "C:\\CommandCenterInstaller\\WinSMTPServer\\WinSMTPServerRelease_0_90_01.zip",
-    source   => "puppet:///modules/puppet_module/WinSMTPServerRelease_0_90_01.zip",
-    owner    => "Administrators",
-    group    => "Administrators",
-    mode     => "0770", 
-    ensure   => present,
-    require  => File["C:\\CommandCenterInstaller\\WinSMTPServer"],
-  }
+  # file { "SMTP Installer" :
+  #   path     => "C:\\CommandCenterInstaller\\WinSMTPServer\\WinSMTPServerRelease_0_90_01.zip",
+  #   source   => "puppet:///modules/puppet_module/WinSMTPServerRelease_0_90_01.zip",
+  #   owner    => "Administrators",
+  #   group    => "Administrators",
+  #   mode     => "0770", 
+  #   ensure   => present,
+  #   require  => File["C:\\CommandCenterInstaller\\WinSMTPServer"],
+  # }
 
-  unzip { "Unzip SMTP setup":
-    source  => "C:\\CommandCenterInstaller\\WinSMTPServer\\WinSMTPServerRelease_0_90_01.zip",
-    creates => "C:\\CommandCenterInstaller\\WinSMTPServer\\WinSMTPServerRelease_0_90_01",
-    require => File["SMTP Installer"],
-    before => Package["SFTP install"],
-  }
+  # unzip { "Unzip SMTP setup":
+  #   source  => "C:\\CommandCenterInstaller\\WinSMTPServer\\WinSMTPServerRelease_0_90_01.zip",
+  #   creates => "C:\\CommandCenterInstaller\\WinSMTPServer\\WinSMTPServerRelease_0_90_01",
+  #   require => File["SMTP Installer"],
+  #   before => Package["SFTP install"],
+  # }
 
   # Install SMTP Server windows feature
   exec { "SMTP Server" :
     command => 'Install-WindowsFeature -Name SMTP-Server',
     timeout => 1200,
     provider => powershell,
-    before => Package["SFTP install"]
   }
 
-  exec { "Install .NET 3.5" :
-    command => 'cmd /c dism /online /enable-feature /featurename:NetFX3 /all /Source:c:\\sources\\sxs /LimitAccess',
-    provider => 'powershell',
-    before => Package["SFTP install"],
-  }
+  # exec { "SMTP Service" :
+  #   command => 'Start-Service SMTPSVC',
+  #   provider => powershell,
+  #   before => Package["SMTP Server"]
+  # }
 
-  package { "SFTP install" :
-    ensure          => installed,
-    source          => "C:\\CommandCenterInstaller\\WinSMTPServer\\setup.exe",
-    install_options => ['/qn'],
-    provider => windows,
-  }
+  # exec { "Install .NET 3.5" :
+  #   command => 'cmd /c dism /online /enable-feature /featurename:NetFX3 /all /Source:c:\\sources\\sxs /LimitAccess',
+  #   provider => 'powershell',
+  #   before => Package["SFTP install"],
+  # }
 
-  service { "WinSMTPService" :
+  # package { "SFTP install" :
+  #   ensure          => installed,
+  #   source          => "C:\\CommandCenterInstaller\\WinSMTPServer\\setup.exe",
+  #   install_options => ['/qn'],
+  #   provider => windows,
+  # }
+
+  service { "SMTPSVC" :
     ensure => running,
-    require => Package['SFTP install']
+    require => Package['SMTP Server']
   }
 
 }
