37a27afc7a5beb25193746968f7a6aea73fef12c
Ramesh Sencha <sencha@Rameshs-MacBook-Pro.local>
2016-04-03 15:04:19 +0530

    Windows feature added : MSMQ

--- manifests/msmq.pp
+++ manifests/msmq.pp
@@ -1,26 +1,32 @@
 class puppet_module::msmq {
 
-  exec { "MSMQ Enable" :
-    command => 'Enable-WindowsOptionalFeature -Online -FeatureName MSMQ -All',
+  exec { "Windows MSMQ Feature Install" :
+    command => 'Get-WindowsFeature -Name MSMQ* | Install-WindowsFeature',
     timeout => 1200,
     provider => powershell
   }
 
-  exec { "MSMQ Services Enable" :
-    command => 'Enable-WindowsOptionalFeature -Online -FeatureName MSMQ-Services -All',
-    timeout => 1200,
-    provider => powershell
-  }
+  # exec { "MSMQ Enable" :
+  #   command => 'Enable-WindowsOptionalFeature -Online -FeatureName MSMQ -All',
+  #   timeout => 1200,
+  #   provider => powershell
+  # }
 
-  exec { "MSMQ Server Enable" :
-    command => 'Enable-WindowsOptionalFeature -Online -FeatureName MSMQ-Server -All',
-    timeout => 1200,
-    provider => powershell
-  }    
+  # exec { "MSMQ Services Enable" :
+  #   command => 'Enable-WindowsOptionalFeature -Online -FeatureName MSMQ-Services -All',
+  #   timeout => 1200,
+  #   provider => powershell
+  # }
 
-  service { "MSMQ" :
-    ensure  => running,
-    require => [ Exec["MSMQ Enable"], Exec["MSMQ Services Enable"], Exec["MSMQ Server Enable"] ]   
-  }
+  # exec { "MSMQ Server Enable" :
+  #   command => 'Enable-WindowsOptionalFeature -Online -FeatureName MSMQ-Server -All',
+  #   timeout => 1200,
+  #   provider => powershell
+  # }    
+
+  # service { "MSMQ" :
+  #   ensure  => running,
+  #   require => [ Exec["MSMQ Enable"], Exec["MSMQ Services Enable"], Exec["MSMQ Server Enable"] ]   
+  # }
 
 }
